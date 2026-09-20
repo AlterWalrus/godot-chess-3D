ccea75c917a69099506282dad1f3509be1461361
AlterWalrus <suraeruarroyo@gmail.com>
2026-09-20 12:18:54 -0600

    update sprites sizes

--- scenes/setup_grid.gd
+++ scenes/setup_grid.gd
@@ -12,8 +12,8 @@ func _ready() -> void:
 	_setup_button(button_sample)
 	for i in range(1, 64):
 		var btn = button_sample.duplicate()
-		var w = !(int(i/8)+i)&1
-		btn.get_node("TextureRect").modulate = Color.WHITE if w else Color.BLACK
+		var col = Color.WHITE if !(int(i/8.0)+i)&1 else Color.BLACK
+		btn.get_node("TextureRect").modulate = col
 		_setup_button(btn)
 		add_child(btn)
 
@@ -25,7 +25,14 @@ func _setup_button(btn: Button) -> void:
 func _button_pressed(btn: Button) -> void:
 	active_button = btn
 	popup.show()
-	popup.position = btn.global_position
+	
+	var offset = Vector2.ZERO
+	var space = get_viewport_rect().size.y - btn.global_position.y
+	if space < popup.size.y:
+		offset.y -= popup.size.y-space
+	#print(btn.global_position.y)
+	#print(get_viewport_rect().size.y)
+	popup.position = btn.global_position + offset
 
 
 func _piece_changed(index: int) -> void:
